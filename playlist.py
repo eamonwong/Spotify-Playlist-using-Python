726b10ad10c23280047ee311d89f36fb39505c89
eamon01 <75913022+eamonwong@users.noreply.github.com>
2023-11-18 00:50:47 +0000

    Update playlist.py

--- playlist.py
+++ playlist.py
@@ -25,7 +25,7 @@ list_of_songs = []
 while user_input.lower() != 'quit':
     result = spotifyObject.search(q=user_input)
     list_of_songs.append(result['tracks']['items'][0]['uri'])
-    user_input = input("Enter the song: ")
+    user_input = input("Enter the song: ")export SPOTIPY_REDIRECT_UR
 
 # Find the newly created playlist for the authenticated user
 pre_playlists = spotifyObject.current_user_playlists()
@@ -33,3 +33,8 @@ playlist = pre_playlists['items'][0]['id']
 
 # Add songs to the authenticated user's playlist
 spotifyObject.playlist_add_items(playlist_id=playlist, items=list_of_songs)
+
+# Have to export these in terminal for project to work
+# export SPOTIPY_CLIENT_ID
+# export SPOTIPY_CLIENT_SECRET
+# export SPOTIPY_REDIRECT_URI
